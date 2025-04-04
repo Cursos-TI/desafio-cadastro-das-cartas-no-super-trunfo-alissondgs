cf46805cbd358ad9ffa186c09513b46491880188
alissondgs <alissongomesdias9876@gmail.com>
2025-04-04 04:39:58 +0000

    Desafio Cartas Super Trunfo

---
+++ teste33.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+    int mes1 = 10;
+    int mes2 = 6;
+    int mes3 =7;
+    int mes4 =5;
+    int mes5 =7;
+    int media = 5;
+
+ int resultado=0;
+
+ resultado +=  mes1+mes2+mes3+mes4+mes5/media;
+
+ printf("o resultado e: %d", resultado);
+
+
+
+
+
+}
